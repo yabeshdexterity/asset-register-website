c78f52f8b629bf432fb1ab24bce0ebc81360a07e
yabeshdexterity <yabesh.dexterity@gmail.com>
2026-09-02 17:19:16 +0530

    Create firebase-config.js

---
+++ assets/firebase-config.js
@@ -0,0 +1,15 @@
+// assets/firebase-config.js
+// Your Firebase configuration from Firebase Console
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
